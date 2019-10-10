1b19aa4735c9c73b4069dd6c765931b20ba47d09
Po Cheng <904830+chengpo@users.noreply.github.com>
2019-10-09 20:19:57 -0700

    Populate EvenlySplittingScreenBehavior

--- app/src/main/java/com/monkeyapp/numbers/EvenlySplittingScreenBehavior.kt
+++ app/src/main/java/com/monkeyapp/numbers/EvenlySplittingScreenBehavior.kt
@@ -35,7 +35,7 @@ import androidx.core.view.forEach
 import androidx.gridlayout.widget.GridLayout
 import com.monkeyapp.numbers.apphelpers.isPortraitMode
 
-class HalfScreenBehavior(context: Context?, attrs: AttributeSet?): CoordinatorLayout.Behavior<ViewGroup>(context, attrs) {
+class EvenlySplittingScreenBehavior(context: Context?, attrs: AttributeSet?): CoordinatorLayout.Behavior<ViewGroup>(context, attrs) {
     override fun onMeasureChild(parent: CoordinatorLayout, child: ViewGroup, parentWidthMeasureSpec: Int, widthUsed: Int, parentHeightMeasureSpec: Int, heightUsed: Int): Boolean {
         var extraHeight = 0
 
