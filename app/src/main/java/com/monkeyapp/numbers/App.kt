79b747befa9324203383d292bfd559a4305cb13b
Po Cheng <904830+chengpo@users.noreply.github.com>
2020-02-08 21:21:19 -0800

    Enable dark mode by following system setting on Android Q and above.

--- app/src/main/java/com/monkeyapp/numbers/App.kt
+++ app/src/main/java/com/monkeyapp/numbers/App.kt
@@ -1,5 +1,6 @@
 package com.monkeyapp.numbers
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDexApplication
 import com.google.android.gms.ads.MobileAds
@@ -9,8 +10,13 @@ class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
 
-        // enable night mode when battery save mode is on
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // enable night mode base on system setting
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        } else {
+            // enable night mode when battery save mode is on
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+        }
 
         // set ads id
         MobileAds.initialize(this, "ca-app-pub-6498719425690429~1480158317")
