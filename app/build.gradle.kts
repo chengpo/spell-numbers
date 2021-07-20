9824d011a080ca8d5716e803bfd7e7e3cbbc089b
po_cheng <904830+chengpo@users.noreply.github.com>
2021-07-20 01:50:18 -0700

    Set versionCode = 43

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 
     defaultConfig {
         applicationId = "com.monkeyapp.numbers"
-        versionCode = 42
-        versionName = "1.0.21.202102250205"
+        versionCode = 43
+        versionName = "1.0.22.202107200149"
 
         minSdk = Config.Android.minSdk
         targetSdk = Config.Android.targetSdk
